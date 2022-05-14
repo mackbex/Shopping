0035da9de688a3f761781e53f5d9e28a21186f3b
Tom Kim <tom.kim@3i.ai>
2022-05-15 08:29:40 +0900

    - added auto-scroll - added Swipe

--- app/src/main/java/com/item/shopping/ui/main/home/banner/BannerAdapter.kt
+++ app/src/main/java/com/item/shopping/ui/main/home/banner/BannerAdapter.kt
@@ -1,34 +1,32 @@
-package com.item.shopping.ui.main.home
+package com.item.shopping.ui.main.home.banner
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.databinding.library.baseAdapters.BR
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.item.shopping.R
-import com.item.shopping.databinding.ItemBannerBinding
+import com.item.shopping.databinding.ItemMainHeaderBannerBinding
 import com.item.shopping.domain.model.Banner
 
 class BannerAdapter: ListAdapter<Banner, BannerAdapter.ViewHolder>(ItemDiffCallback()) {
 
-    private var listener: ((banner:Banner, binding:ViewDataBinding) -> Unit)? = null
     private var actualSize = 0
-
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             DataBindingUtil.inflate(
                 LayoutInflater.from(parent.context),
-                R.layout.item_banner,
+                R.layout.item_main_header_banner,
                 parent,
                 false
             )
         )
     }
+
 
     override fun submitList(list: List<Banner>?) {
         val infiniteList = list?.let {
@@ -40,21 +38,16 @@ class BannerAdapter: ListAdapter<Banner, BannerAdapter.ViewHolder>(ItemDiffCallb
         super.submitList(infiniteList)
     }
 
-
     fun actualItemCount():Int = actualSize
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(getItem(position % actualItemCount()))
     }
 
-    fun setPostInterface(listener: ((banner:Banner, binding:ViewDataBinding) -> Unit)?) {
-        this.listener = listener
-    }
 
-    inner class ViewHolder(private val binding:ItemBannerBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(banner:Banner) {
-            binding.setVariable(BR.banner, banner)
-            listener?.invoke(banner, binding)
+    inner class ViewHolder(private val binding:ItemMainHeaderBannerBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(banners:Banner) {
+            binding.setVariable(BR.banner, banners)
             binding.executePendingBindings()
         }
     }
